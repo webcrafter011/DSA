156c03393845ab24d139a0243d4921078bdf95c5
webcrafter011 <manujchaudhari123@gmail.com>
2024-12-06 22:06:52 +0530

    updated queueImplementationArr.py

--- Stack and Queue/queueImplementationArr.py
+++ Stack and Queue/queueImplementationArr.py
@@ -1,27 +1,22 @@
 class Queue:
     def __init__(self):
+        # Initializes an empty queue using a list
         self.array = []
 
     def peek(self):
-        return self.array[0]
+        if len(self.array) > 0:
+            return self.array[0]
+        return None  # Return None if the queue is empty
 
     def enqueue(self, data):
         self.array.append(data)
 
     def dequeue(self):
-        self.array.pop(0)
-        
+        if len(self.array) > 0:
+            return self.array.pop(0)
+        return None  # Return None if the queue is empty
+
     def print_q(self):
         for i in self.array:
             print(i, end=' -> ')
         print("None")
-        return
-
-newq = Queue()
-newq.enqueue(10)
-newq.enqueue(20)
-newq.enqueue(30)
-newq.enqueue(40)
-newq.dequeue()
-newq.dequeue()
-newq.print_q()
