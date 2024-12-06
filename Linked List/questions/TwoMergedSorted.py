ec94df7a68ddf0e7778beb8c7e57d48bcc405c2e
webcrafter011 <manujchaudhari123@gmail.com>
2024-12-06 10:49:41 +0530

    updated TwoMergedSorted.py

--- Linked List/questions/TwoMergedSorted.py
+++ Linked List/questions/TwoMergedSorted.py
@@ -1,10 +1,42 @@
-# Given are two sorted linked lists
-# TODO: we have to create a new sorted merged linked list 
-
-class Node(objecy):
-    def __init__(self, value = 0, next = None):
-        self.value = value
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
         self.next = next
-        
-class Solution(object):
-    
+
+class Solution:
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if not list1:  # Handle empty lists
+            return list2
+        if not list2:
+            return list1
+
+        # Determine the head of the merged list
+        if list1.val < list2.val:
+            head = list1
+            list1 = list1.next
+        else:
+            head = list2
+            list2 = list2.next
+
+        # Tail pointer starts at the head
+        tail = head
+
+        # Merge the lists
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next  # Move the tail pointer forward
+
+        # Attach the remaining nodes (if any)
+        tail.next = list1 if list1 else list2
+
+        return head
