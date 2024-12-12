660b72470b82fd17cb7cafce0d7f0ac473e12c0e
webcrafter011 <manujchaudhari123@gmail.com>
2024-12-12 13:26:39 +0530

    Added implementation of quicksort in the proper file

--- Algorithms/Sorting/quickSort.py
+++ Algorithms/Sorting/quickSort.py
@@ -1,11 +1,13 @@
-def quickSort(array):
-    n = len(array)
-    pivit = array[n - 1]
+def quick_sort(arr):
+    if len(arr) <= 1:  # Base case: a list of 0 or 1 elements is already sorted
+        return arr
+    else:
+        # Choose a pivot (typically the last element)
+        pivot = arr[-1]
+        # Partition the array into elements less than the pivot and greater than or equal to the pivot
+        less_than_pivot = [x for x in arr[:-1] if x <= pivot]
+        greater_than_pivot = [x for x in arr[:-1] if x > pivot]
+        # Recursively sort partitions and combine them
+        return quick_sort(less_than_pivot) + [pivot] + quick_sort(greater_than_pivot)
 
-    for i in 
-
-
-
-
-
-
+    
